f0d16fee2e8ae3f04ef385cf8adcb47472875999
Marko Laurits <marko.laurits@testijad.ee>
2023-06-09 14:03:21 +0300

    upload everything

---
+++ tests/__tests__/movie-tests.js
@@ -0,0 +1,42 @@
+const {expect, describe, test} = require('@jest/globals')
+const request = require("supertest")
+const movies = require("../test_data/movies.json")
+
+let baseUri = process.env.BASE_URI ?? "http://localhost"
+let port = process.env.PORT ?? 3000
+
+let testUri = port ? `${baseUri}:${port}` : baseUri
+
+module.exports = {
+	testEnvironment: "node",
+}
+
+describe("Movie API endpoint tests", () => {
+
+    test("GET /api/movies", (done) => {
+        request(testUri)
+            .get('/api/movies')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.length).toBeGreaterThan(1)
+                expect(res.body).toEqual(movies)
+            })
+            .end((err, res) => {
+                if (err) return done(err);
+                return done();
+            })
+    })
+
+    test("GET /api/movies/title", (done) => {
+        request(testUri)
+            .get('/api/movies/title/ET')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.title).toEqual("ET")
+            })
+            .end((err, res) => {
+                if (err) return done(err);
+                return done();
+            })
+    })
+})
